ac3af1717a2622ec7496b54d2304b0e713dd2fa8
tladams <adams3tl@dukes.jmu.edu>
2022-11-14 12:56:48 -0500

    Add files via upload
    adding files to start lab

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1. append text to li that has which li it is on the page (i.e. 0..n-1)
+// start by getting references to the relevant DOM elements
+// make the specified modifications
+
+// 2. when click div console.log(which div it is on the page, i.e. 0..n-1)
